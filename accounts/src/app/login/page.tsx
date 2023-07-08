df233df56cd2f93a7899bf9b4d58a3d10d1b6d2e
Evan Gruère <evan.gruere@gmail.com>
2023-07-08 12:00:18 +0200

    Add Kratos workflow to Accounts

---
+++ accounts/src/app/login/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import ory from '../../ory';
+import { useRouter } from 'next/navigation';
+import { AxiosError } from 'axios';
+
+export default function Home() {
+  const [session, setSession] = useState<string>(
+    "No valid Ory Session was found.\nPlease sign in to receive one.",
+  )
+  const [hasSession, setHasSession] = useState<boolean>(false);
+  const router = useRouter();
+  useEffect(() => {
+    ory
+      .toSession()
+      .then(({ data }) => {
+        setSession(JSON.stringify(data, null, 2))
+        setHasSession(true)
+      })
+      .catch((err: AxiosError) => {
+        switch (err.response?.status) {
+          case 403:
+          // This is a legacy error code thrown. See code 422 for
+          // more details.
+          case 422:
+            // This status code is returned when we are trying to
+            // validate a session which has not yet completed
+            // its second factor
+            return router.push("/login?aal=aal2")
+          case 401:
+            // do nothing, the user is not logged in
+            return
+        }
+
+        // Something else happened!
+        return Promise.reject(err)
+      })
+  }, [router])
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello
+    </main>
+  )
+}
---
+++ accounts/src/ory.ts
@@ -0,0 +1,6 @@
+import { Configuration, FrontendApi } from "@ory/client"
+import { edgeConfig } from "@ory/integrations/next"
+
+const frontendApi = new FrontendApi(new Configuration(edgeConfig));
+
+export default frontendApi;
