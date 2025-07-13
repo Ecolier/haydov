3018705387bc0d1dfe2d4198365cd7a62bcbf30f
Evan Gruère <evan.gruere@gmail.com>
2025-07-13 22:12:25 +0200

    feat: added Helm deployment of RabbitMQ

--- services/geography/storage/scripts/configure.sh
+++ services/geography/storage/scripts/configure.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-set -e
-
-wait_until_ready() {
-  until curl -s http://localhost:$MINIO_CONSOLE_PORT/minio/health/ready; do
-    sleep 1
-  done
-}
-
-wait_until_ready
-
-# Setup alias for mc
 mc alias set local http://localhost:$MINIO_API_PORT "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
 
 # Create bucket if not exists
---
+++ services/geography/storage/scripts/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+export MINIO_NOTIFY_AMQP_URL_PRIMARY="amqp://$MESSAGE_BROKER_DEFAULT_USER:$MESSAGE_BROKER_DEFAULT_PASS@$MESSAGE_BROKER_ENDPOINT"
+echo "Using AMQP URL: $MINIO_NOTIFY_AMQP_URL_PRIMARY"
+
+minio server --console-address :9001 /data & MINIO_PID=$!
+
+{
+until curl -s http://localhost:$MINIO_CONSOLE_PORT/minio/health/ready; do
+  sleep 1
+done
+
+./scripts/configure.sh
+} &
+
+wait $MINIO_PID
+
